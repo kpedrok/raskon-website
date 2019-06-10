7b608dd1a7afa73489681eb11d81a30587823d4f
unknown <pedrokvb@gmail.com>
2019-06-10 20:58:51 -0300

    api dns

--- terraform/site.tf
+++ terraform/site.tf
@@ -176,3 +176,15 @@ resource "aws_route53_record" "root" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "api" {
+  zone_id = "${aws_route53_zone.zone.zone_id}"
+  name    = "${aws_api_gateway_domain_name.domain_name.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_api_gateway_domain_name.domain_name.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.domain_name.cloudfront_zone_id}"
+    evaluate_target_health = true
+  }
+}
